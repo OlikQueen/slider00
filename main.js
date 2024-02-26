f78aeca1625bd1b545f4703cc6c467ddf49859a1
Olivia <olysoz@mail.ru>
2024-02-26 16:56:03 +0300

    callback-задачи

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import { slider } from "./src/slider";
 import './src/scss/style.scss';
 
+// import { callback } from "./src/callback"
+// callback();
+
 slider();
-export default slider;
+export default slider; // для загрузки глобально
---
+++ src/callback.js
@@ -0,0 +1,37 @@
+export const callback = () => {
+    // function coll() {
+    //     console.log("матрёшка");
+    // };
+    // function bar(fanColl) {
+    //     fanColl();
+    // };
+    // bar(coll);
+
+
+
+    const pi = 3.14;
+    const str = 'Мир'
+    // const obgect = {
+    //     name: "Olik"
+    // };
+
+    // const ar = [1,2,3]
+    function a() {
+        
+    }
+
+
+    function foo(arg1) {
+        
+        console.log(arg1(( ) => { return 5}));
+
+
+    }
+
+
+    // foo(pi);
+    // foo(str);
+    // foo({name: "Olik"});
+    // foo([1,2,3]);
+     foo((argyment) => {return argyment});
+}
